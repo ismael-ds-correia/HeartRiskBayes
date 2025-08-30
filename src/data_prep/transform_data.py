c727d11f2ba5eeceb3782cc8d7eac24f0c7cb82b
unknown <allan2021jose@gmail.com>
2025-08-30 19:17:44 -0300

    Corrigido transformação de etnia

--- src/data_prep/transform_data.py
+++ src/data_prep/transform_data.py
@@ -12,6 +12,9 @@ for line in lines:
     line = line.replace('Low', '0').replace('Medium', '2').replace('High', '3')
     line = line.replace('18-24', '0').replace('25-29', '1').replace('30-34', '2').replace('35-39', '3').replace('40-44', '4').replace('45-49', '5').replace('50-54', '6').replace('55-59', '7').replace('60-64', '8').replace('65-69', '9').replace('70-74', '10').replace('75-79', '11').replace('80 or older', '12')
     line = line.replace('Excellent', '4').replace('Very good', '3').replace('Good', '2').replace('Fair', '1').replace('Poor', '0')
+    line = line.replace('borderline diabetes', '2').replace('Borderline diabetes', '2')
+    line = line.replace('borderline', '2').replace('Borderline', '2')
+    line = line.replace('Yes (during pregnancy)', '3').replace('yes (during pregnancy)', '3')
     new_lines.append(line)
 
 with open(output_path, 'w', encoding='utf-8') as f:
